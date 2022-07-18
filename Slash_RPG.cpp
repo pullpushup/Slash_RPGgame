67d41da0debd1ac1818ac293353ce94c5d26b0d4
unknown <dman1999q@gmail.com>
2022-07-18 15:15:50 +0200

    added character movement

--- Slash_RPG.cpp
+++ Slash_RPG.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 int main()
 {
     //Window Dimensions
@@ -7,15 +8,40 @@ int main()
     InitWindow(windowWidth,windowHeight,"Slash The RPG Game");
 
     Texture2D map = LoadTexture("WorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0}; 
+
+    Texture2D Player = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 PlayerPos{
+         (float)windowWidth/2.0f - 4.0f * (0.5f * (float)Player.width/6.0f),
+         (float)windowHeight/2.0f - (0.5f * (float)Player.height)
+    };
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        Vector2 mapPos{0.0, 0.0};
+        
+        Vector2 direction{};
+        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if(IsKeyDown(KEY_D)) direction.x += 1.0;
+        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if(IsKeyDown(KEY_S)) direction.y += 1.0;
+        if(Vector2Length(direction) != 0.0)
+        {
+            //set mapPos = mapPose - direction "noted here"
+           
+            mapPos = Vector2Subtract(mapPos,Vector2Scale( Vector2Normalize(direction), speed));
+        }
+        
+        // draw a map here
         DrawTextureEx(map, mapPos, 0.0, 3.0, WHITE);
-
+     
+        //draw the character
+        Rectangle source{0.f, 0.f, (float)Player.width/6.f, (float)Player.height};
+        Rectangle dest{PlayerPos.x, PlayerPos.y, 4.0f * Player.width/6.0f, 4.0f * (float)Player.height};
+        DrawTexturePro(Player, source, dest, Vector2{}, 0.f, WHITE);
         EndDrawing();
     }
     CloseWindow();
