2bc9a679af2cc59e7f6cb95eb17bf3c24fc1eb67
unknown <dman1999q@gmail.com>
2022-07-18 22:05:44 +0200

    added new header and onstructiosn

---
+++ Character.h
@@ -0,0 +1,28 @@
+#include "raylib.h"
+
+class Character
+{
+public:
+
+    Character();
+    Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
+
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos;
+    Vector2 worldPos;
+    // 1: facing right and left -1
+    float rightLeft{1.f};
+    // animation of varibles
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float updateTime{1.f / 12.f};
+    float speed{4.f};
+    float width{};
+    float height{};
+};
