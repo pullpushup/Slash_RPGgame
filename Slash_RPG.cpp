92f48fa6c69ce2415ee54162cb6cca2fc2325e25
unknown <dman1999q@gmail.com>
2022-07-18 21:09:18 +0200

    new instances

--- Slash_RPG.cpp
+++ Slash_RPG.cpp
@@ -1,49 +1,104 @@
 #include "raylib.h"
 #include "raymath.h"
+
+class Character
+{
+public:
+    Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
+
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos;
+    Vector2 worldPos;
+    // 1: facing right and left -1
+    float rightLeft{1.f};
+    // animation of varibles
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f / 12.f};
+    const float speed{4.f};
+};
+
+    void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {
+        (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f),
+        (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)
+    };
+}
+
+void Character::tick(float deltaTime)
+{
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set WorldPos = WorldPose + direction "noted here for world map"
+
+        worldPos = Vector2Subtract(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+    // update the character frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    //draw the character
+    Rectangle source{frame * (float)texture.width / 6.f,0.f, rightLeft * (float)texture.width / 6.f, (float)texture.height};
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f *(float)texture.width /6.0f, 4.0f* (float)texture.height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f,WHITE);
+}
+
 int main()
 {
-    //Window Dimensions
+    // Window Dimensions
     const int windowWidth{720};
     const int windowHeight(720);
-    InitWindow(windowWidth,windowHeight,"Slash The RPG Game");
+    InitWindow(windowWidth, windowHeight, "Slash The RPG Game");
 
     Texture2D map = LoadTexture("WorldMap.png");
     Vector2 mapPos{0.0, 0.0};
-    float speed{4.0}; 
 
-    Texture2D Player = LoadTexture("characters/knight_idle_spritesheet.png");
-    Vector2 PlayerPos{
-         (float)windowWidth/2.0f - 4.0f * (0.5f * (float)Player.width/6.0f),
-         (float)windowHeight/2.0f - (0.5f * (float)Player.height)
-    };
+   Character player;
+    player.setScreenPos(windowWidth, windowHeight);
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        
-        Vector2 direction{};
-        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if(IsKeyDown(KEY_D)) direction.x += 1.0;
-        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if(IsKeyDown(KEY_S)) direction.y += 1.0;
-        if(Vector2Length(direction) != 0.0)
-        {
-            //set mapPos = mapPose - direction "noted here"
-           
-            mapPos = Vector2Subtract(mapPos,Vector2Scale( Vector2Normalize(direction), speed));
-        }
-        
+
+        mapPos = Vector2Scale(player.getWorldPos(), -1.f);
+
         // draw a map here
         DrawTextureEx(map, mapPos, 0.0, 3.0, WHITE);
-     
-        //draw the character
-        Rectangle source{0.f, 0.f, (float)Player.width/6.f, (float)Player.height};
-        Rectangle dest{PlayerPos.x, PlayerPos.y, 4.0f * Player.width/6.0f, 4.0f * (float)Player.height};
-        DrawTexturePro(Player, source, dest, Vector2{}, 0.f, WHITE);
+        player.tick(GetFrameTime());
+
         EndDrawing();
+
     }
     CloseWindow();
-    
 }
